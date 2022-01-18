e05834cf40c3853693fedf3b5fbb6a1843dba0b3
tanghengchao <https://git.integrity.com.cn/>
2022-01-18 17:40:02 +0800

    黑马netty-完结

--- netty/src/main/java/com/nx/netty/heima/day02/Server01.java
+++ netty/src/main/java/com/nx/netty/heima/day02/Server01.java
@@ -8,6 +8,8 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -27,14 +29,24 @@ public class Server01 {
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
                         // 解码 ByteBuf => String
                         ch.pipeline().addLast(new StringDecoder());
                         ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
                             @Override
                             protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+                                //PooledUnsafeDirectByteBuf 默认池化和直接内存
+                                //-Dio.netty.allocator.type=unpooled -Dio.netty.noPreferDirect=true 可调整为非池化和堆内存
+                                log.debug("ctx-buf: " + ctx.alloc().buffer());
                                 log.debug("msg: " + msg);
                             }
                         });
+//                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
+//                            @Override
+//                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//                                ctx.fireChannelRead(msg);
+//                            }
+//                        });
                     }
                 })
                 // ServerSocketChannel 绑定的监听端口
